13825f3e4874e13caa25c45ab32e16dcc3d279ab
Vojtech Vitek <vojtech.vitek@golang.cz>
2022-11-02 12:14:30 +0100

    Add JS client example from webrpc repo

---
+++ _examples/hello-webrpc/server/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+)
+
+func main() {
+	err := startServer()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func startServer() error {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	cors := cors.New(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"POST", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	r.Use(cors.Handler)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("."))
+	})
+
+	webrpcHandler := NewExampleServiceServer(&ExampleServiceRPC{})
+	r.Handle("/*", webrpcHandler)
+
+	return http.ListenAndServe(":4242", r)
+}
+
+type ExampleServiceRPC struct {
+}
+
+func (s *ExampleServiceRPC) Ping(ctx context.Context) (bool, error) {
+	return true, nil
+}
+
+func (s *ExampleServiceRPC) GetUser(ctx context.Context, userID uint64) (*User, error) {
+	if userID == 911 {
+		return nil, ErrorNotFound("unknown userID %d", 911)
+		// return nil, webrpc.Errorf(webrpc.ErrNotFound, "unknown userID %d", 911)
+		// return nil, webrpc.WrapError(webrpc.ErrNotFound, err, "unknown userID %d", 911)
+	}
+
+	return &User{
+		ID:       userID,
+		Username: "hihi",
+	}, nil
+}
